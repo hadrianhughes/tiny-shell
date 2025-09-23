291b1eaae7369c1961a643e0c66466c69d2f7479
Hadrian Hughes <hadrianhughes@gmail.com>
2025-09-23 18:52:49 +0100

    Create a function to parse tokens from stdin input

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define LINE_BUF_SIZE 1024
+#define TOKEN_BUF_SIZE 64
+#define TOKEN_DELIM_CHARS " \t\n\r\a"
 
 char *read_line(void) {
   size_t buf_size = LINE_BUF_SIZE;
@@ -20,11 +23,50 @@ char *read_line(void) {
   return buffer;
 }
 
-int main(int argc, char **argv) {
+char **split_tokens(char *line) {
+  size_t buf_size = TOKEN_BUF_SIZE;
+  size_t num_tokens = 0;
 
+  char **tokens = malloc(sizeof(char*) * buf_size);
+  if (!tokens) {
+    perror("tsh: failed to allocate tokens array");
+    exit(EXIT_FAILURE);
+  }
+
+  char *current_token = strtok(line, " \t\r\n\a");
+
+  while (current_token != NULL) {
+    tokens[num_tokens++] = current_token;
+
+    if (num_tokens >= buf_size) {
+      buf_size += TOKEN_BUF_SIZE;
+      tokens = realloc(tokens, sizeof(char*) * buf_size);
+
+      if (!tokens) {
+        perror("tsh: failed to reallocate tokens array");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    current_token = strtok(NULL, TOKEN_DELIM_CHARS);
+  }
+
+  tokens[num_tokens] = NULL;
+
+  return tokens;
+}
+
+int main() {
   char *line = read_line();
-  printf("%s\n", line);
+  char **tokens = split_tokens(line);
+
+  size_t position = 0;
+  while (tokens[position] != NULL) {
+    printf("%s\n", tokens[position++]);
+  }
+
   free(line);
+  free(tokens);
 
   return EXIT_SUCCESS;
 }
