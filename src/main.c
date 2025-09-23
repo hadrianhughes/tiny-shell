65ca2c09a3a51a3f30e46642fe20cd963b7278b3
Hadrian Hughes <hadrianhughes@gmail.com>
2025-09-23 19:32:30 +0100

    Create a function to execute a program from the given command

--- src/main.c
+++ src/main.c
@@ -1,16 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "read.h"
+
+void run_program(char **args) {
+  pid_t pid = fork();
+  if (pid < 0) {
+    perror("tsh: failed to fork process");
+    exit(EXIT_FAILURE);
+  }
+
+  if (pid == 0) {
+    // child process
+    if (execvp(args[0], args) == -1) {
+      perror("tsh: failed to execute command");
+    }
+
+    exit(EXIT_FAILURE);
+  } else {
+    // parent process
+    int status;
+    pid_t wpid;
+
+    do {
+      wpid = waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(wpid) && !WIFSIGNALED(wpid));
+  }
+}
 
 int main() {
   char *line = read_line();
   char **tokens = split_tokens(line);
 
-  size_t position = 0;
-  while (tokens[position] != NULL) {
-    printf("%s\n", tokens[position++]);
-  }
+  run_program(tokens);
 
   free(tokens);
   free(line);
