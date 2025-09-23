da44b97c46fa0e74fe307abc81b0113809d62fdd
Hadrian Hughes <hadrianhughes@gmail.com>
2025-09-23 18:04:56 +0100

    Create a function to read a line from stdin

--- src/main.c
+++ src/main.c
@@ -1,10 +1,30 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) {
-  for (int i = 0;i < argc;i++) {
-    printf("%s\n", argv[i]);
+#define LINE_BUF_SIZE 1024
+
+char *read_line(void) {
+  size_t buf_size = LINE_BUF_SIZE;
+  char *buffer = NULL;
+
+  if (getline(&buffer, &buf_size, stdin) == -1) {
+    if (feof(stdin)) {
+      exit(EXIT_SUCCESS);
+    } else {
+      perror("tsh: failed to read line from stdin");
+      exit(EXIT_FAILURE);
+    }
   }
+
+  return buffer;
+}
+
+int main(int argc, char **argv) {
+
+  char *line = read_line();
+  printf("%s\n", line);
+  free(line);
 
   return EXIT_SUCCESS;
 }
