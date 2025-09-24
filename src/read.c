b252398cefe681ba7e2e5adb86fe2a15613d4722
Hadrian Hughes <hadrianhughes@gmail.com>
2025-09-24 19:27:29 +0100

    Write a custom tokenizer function that accounts for double quotes

--- src/read.c
+++ src/read.c
@@ -25,35 +25,63 @@ char *read_line(void) {
   return buffer;
 }
 
+bool is_ws_char(char c) {
+  return c == ' ' || c == '\n' || c == '\r';
+}
+
 char **tokenize(char *line) {
   size_t buf_size = TOKEN_BUF_SIZE;
   size_t num_tokens = 0;
 
-  char **tokens = malloc(sizeof(char*) * buf_size);
+  char **tokens = (char **) malloc(sizeof(char *) * buf_size);
   if (!tokens) {
     perror("tsh: failed to allocate tokens array");
     exit(EXIT_FAILURE);
   }
 
-  char *current_token = strtok(line, " \t\r\n\a");
+  char *token_start = NULL;
 
-  while (current_token != NULL) {
-    tokens[num_tokens++] = current_token;
+  bool in_quotes = false;
+
+  for (char *p = line;; p++) {
+    char c = *p;
+
+    if (c == '\0') {
+      if (token_start) {
+        tokens[num_tokens++] = token_start;
+        token_start = NULL;
+      }
+
+      break;
+    }
+
+    if (c == '"') {
+      if (in_quotes) {
+        *p = '\0';
+        tokens[num_tokens++] = token_start;
+        token_start = NULL;
+      }
+
+      in_quotes = !in_quotes;
+
+      continue;
+    }
+
+    if (is_ws_char(c)) {
+      if (token_start) {
+        *p = '\0';
+        tokens[num_tokens++] = token_start;
+        token_start = NULL;
+      }
+    } else if (!token_start) {
+      token_start = p;
+    }
 
     if (num_tokens >= buf_size) {
       buf_size += TOKEN_BUF_SIZE;
-      tokens = realloc(tokens, sizeof(char*) * buf_size);
-
-      if (!tokens) {
-        perror("tsh: failed to reallocate tokens array");
-        exit(EXIT_FAILURE);
-      }
+      tokens = (char **) realloc(tokens, sizeof(char *) * buf_size);
     }
-
-    current_token = strtok(NULL, TOKEN_DELIM_CHARS);
   }
-
-  tokens[num_tokens] = NULL;
 
   return tokens;
 }
