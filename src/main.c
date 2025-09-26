f163c6c1595dfa1171f526e9ee4ff4c171a10e20
Hadrian Hughes <hadrianhughes@gmail.com>
2025-09-26 19:15:38 +0100

    Add GNU readline to the project and use for reading input

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 #include "read.h"
 #include "tsh.h"
@@ -9,10 +11,11 @@ int main() {
   tsh_status_t status;
 
   do {
-    print_prompt();
-    char *line = read_line();
-    char **tokens = tokenize(line);
+    char *line = readline("tsh ~> ");
 
+    add_history(line);
+
+    char **tokens = tokenize(line);
     status = run_cmd(tokens);
 
     free(tokens);
